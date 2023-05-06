3b199a054dac343c7df089466be8945da9f49f17
KonstantYa <lakaste@bk.ru>
2023-05-06 10:02:35 +0300

    lesson#1

---
+++ lesson#1/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Console.WriteLine("enter a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter a number ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number==number2*number2)
+{
+   Console.WriteLine("число 1 является квадратом числа 2");
+}
+else
+{
+   Console.WriteLine("число 1 не является квадратом числа 2");
+}
+
+Console.WriteLine("enter a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number==1)
+{
+   Console.WriteLine("Понедильник");
+}
+else if (number==2)
+{
+   Console.WriteLine("Вторник");
+}
+else if (number==3)
+{
+   Console.WriteLine("Среда");
+}
+else if (number==4)
+{
+   Console.WriteLine("Четверг");
+}
+else if (number==5)
+{
+   Console.WriteLine("Пятница");
+}
+else if (number==6)
+{
+   Console.WriteLine("Суббота");
+}
+else if (number==7)
+{
+   Console.WriteLine("Воскресенье");
+}
+
+else 
+{
+   Console.WriteLine("ERROR");
+}
+
+
+Console.WriteLine("enter a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Negnumber =- number;
+
+while(number>=Negnumber)
+
+{
+   Console.WriteLine(Negnumber);
+   Negnumber++;
+}
+
+Console.WriteLine("enter a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number%10;
+Console.WriteLine(result);
+*/
