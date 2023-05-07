2daa4857c431272ef9a55cddaaedc1568829a18f
KonstantYa <lakaste@bk.ru>
2023-05-07 10:39:29 +0300

    add lesson#2

---
+++ lesson#2/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+int t = 6;
+//Канкатинация
+Console.Write("Наше число " + t + t );
+
+//Интерполяция
+Console.Write($"Наше число {t} {t} Lorem" );
+*/
+
+
+//Рандомные числа
+/*
+int num = new Random ().Next(10,100);
+Console.WriteLine(num);
+int a = num /10;
+int b = num%10;
+
+if(a>b)
+
+{
+   Console.WriteLine(a);
+}
+else if(a<b)
+
+{
+   Console.WriteLine(b);
+}
+else
+
+{
+   Console.WriteLine("Числа равны");
+}
+*/
+
+/*
+int num = new Random ().Next(100,1000);
+Console.WriteLine(num);
+int a = num /100;
+int b = num%10;
+int c = (num % 100) / 10;
+
+
+Console.WriteLine("Итоговое число " + a + b);
+//Console.WriteLine($"Итоговое число {a} {b}");
+//Console.WriteLine("Итоговое число " + c);
+*/
+
+
+// Console.WriteLine("Enter a first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter a second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int result = num1 % num2;
+
+// if (result == 0)
+// {
+//    Console.WriteLine("Число 2 кратно числу 1");
+// }
+// else
+// {
+//    Console.WriteLine("Число 2 не кратно числу 1 " + "остаток " + result);
+// }
+
+
+// int num = new Random().Next(10, 1000);
+// Console.WriteLine(num);
+
+// if ((num%7==0)&&(num%23==00))
+// {
+//    Console.WriteLine ("Да");
+// }
+// else
+// {
+// Console.WriteLine("Нет");
+// }
+
+// Console.WriteLine("Enter a first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter a second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 == num2*num2)
+// {
+//    Console.WriteLine("Число 2 является квадратом числа 1");
+// }
+// else if (num2 == num1*num1)
+// {
+//    Console.WriteLine("Число 1 является квадратом числа 2");
+// }
+// else
+// {
+//    Console.WriteLine ("Числа не совместимы");
+// }
+
