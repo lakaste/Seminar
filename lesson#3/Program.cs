c70588984f179bc1853550d3a31190b2734cc1f3
KonstantYa <lakaste@bk.ru>
2023-05-07 20:53:36 +0300

    add switch

--- lesson#3/Program.cs
+++ lesson#3/Program.cs
@@ -24,3 +24,100 @@
 // {
 // Console.WriteLine ("Точка находится на одной или двух координат");
 // }
+
+
+// Console.WriteLine ("Enter a first number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+// if(num ==1)
+//  {
+//  Console.WriteLine ("1 Четверть");
+//  Console.WriteLine ("x>0 && y>0");
+//  }
+//  else if(num ==2)
+//  {
+//  Console.WriteLine ("2 Четверть");
+//   Console.WriteLine ("x<0 && y>0");
+//  }
+//  else if(num ==3)
+//  {
+//  Console.WriteLine ("3 Четверть");
+//  Console.WriteLine ("x<0 && y<0");
+//  }
+//  else if(num ==4)
+//  {
+//  Console.WriteLine ("4 Четверть");
+//  Console.WriteLine ("x>0 && y<0");
+//  }
+//  else
+//  {
+//  Console.WriteLine ("Точка находится на одной или двух координат");
+//  }
+
+
+
+Console.WriteLine ("Enter a first number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch(num)
+{
+   case 1: 
+   {
+     Console.WriteLine ("1 Четверть");
+     Console.WriteLine ("x>0 && y>0");
+     break;
+   }
+      case 2: 
+   {
+     Console.WriteLine ("2 Четверть");
+     Console.WriteLine ("x<0 && y>0");
+     break;
+   }
+      case 3: 
+   {
+    Console.WriteLine ("3 Четверть");
+    Console.WriteLine ("x<0 && y<0");
+     break;
+   }
+      case 4: 
+   {
+    Console.WriteLine ("4 Четверть");
+     Console.WriteLine ("x>0 && y<0");
+     break;
+   }
+   default:
+   {
+      
+   Console.WriteLine ("такой четверти нет");
+   break;
+   }
+}
+
+
+
+// if(num ==1)
+//  {
+//  Console.WriteLine ("1 Четверть");
+//  Console.WriteLine ("x>0 && y>0");
+//  }
+//  else if(num ==2)
+//  {
+//  Console.WriteLine ("2 Четверть");
+//   Console.WriteLine ("x<0 && y>0");
+//  }
+//  else if(num ==3)
+//  {
+//  Console.WriteLine ("3 Четверть");
+//  Console.WriteLine ("x<0 && y<0");
+//  }
+//  else if(num ==4)
+//  {
+//  Console.WriteLine ("4 Четверть");
+//  Console.WriteLine ("x>0 && y<0");
+//  }
+//  else
+//  {
+//  Console.WriteLine ("Точка находится на одной или двух координат");
+//  }
