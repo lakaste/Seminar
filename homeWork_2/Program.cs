3e55625447dcc048691b709302084cebffa2d02c
KonstantYa <lakaste@bk.ru>
2023-05-07 10:47:18 +0300

    add exercise_10

---
+++ homeWork_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter a first number");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = (num % 100) / 10;
+
+
+Console.WriteLine("Итоговое число " + a);
+
