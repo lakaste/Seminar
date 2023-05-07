4510e85d758d195a733d91620de1bb737c073c98
KonstantYa <lakaste@bk.ru>
2023-05-07 18:53:53 +0300

    first_exercise

--- lesson#3/Program.cs
+++ lesson#3/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Console.WriteLine ("Enter a first number");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Enter a first number");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+
+// if(x>0 && y>0)
+// {
+// Console.WriteLine ("1 Четверть");
+// }
+// else if(x<0 && y>0)
+// {
+// Console.WriteLine ("2 Четверть");
+// }
+// else if(x<0 && y<0)
+// {
+// Console.WriteLine ("3 Четверть");
+// }
+// else if(x>0 && y<0)
+// {
+// Console.WriteLine ("4 Четверть");
+// }
+// else
+// {
+// Console.WriteLine ("Точка находится на одной или двух координат");
+// }
