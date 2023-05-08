024a580f8eeecc973206aa0caa86b42372668330
KonstantYa <lakaste@bk.ru>
2023-05-08 07:47:58 +0300

    add exercise_1

---
+++ lesson#4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Console.WriteLine("Enter a number");
+// int numbers = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// int count = 0;
+
+// int GetSumNum()
+// {
+//   while (numbers>count)
+//   {
+//    count++;
+//    sum+=count;
+//   }
+
+//    return sum;
+// }
+
+// int result = GetSumNum();
+// Console.WriteLine(result); 
+
