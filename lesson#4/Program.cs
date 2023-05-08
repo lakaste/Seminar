ee9528466a7f0c95beef8504ebbdd0216c07322a
KonstantYa <lakaste@bk.ru>
2023-05-08 08:52:17 +0300

    add all exercise

--- lesson#4/Program.cs
+++ lesson#4/Program.cs
@@ -23,12 +23,22 @@
 
 
 
-Console.WriteLine("Enter a number");
-int num = Convert.ToInt32(Console.ReadLine());
-int temp = 1;
+// Console.WriteLine("Enter a number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int temp = 1;
 
-for (int i = 1; i!=num+1;i++)
-{
-   temp = temp*i;
-}
-Console.WriteLine(temp);
+// for (int i = 1; i!=num+1;i++)
+// {
+//    temp = temp*i;
+// }
+// Console.WriteLine(temp);
+
+// int [] numArr = new int [8];
+// for (int i=0;i<numArr.Length;i++)
+// {
+//    numArr[i] = new Random().Next(0, 2);
+//    // Console.WriteLine(numArr[i]);
+//    //Console.Write(numArr[i] + ",");
+// }
+
+//    Console.WriteLine(String.Join(",", numArr));
