c02a81d6feb4574f854adff52a416a31b738677e
KonstantYa <lakaste@bk.ru>
2023-05-09 17:44:55 +0300

    first_commit

---
+++ lesson#5/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int[] numArr = new int[12];
+int positiveNum=0;
+int negNum=0;
+for(int i=0;i<numArr.Length;i++)
+{
+   numArr[i] = new Random().Next(-9, 10);
+
+   if(numArr[i]>0)
+   {
+      positiveNum+=numArr[i];
+   }
+   else
+   {
+      negNum+=numArr[i];
+   }
+}
+
+Console.WriteLine(String.Join(",", numArr)+ " \nсумма положительных "+positiveNum+" сумма отрицательных "+ negNum);
