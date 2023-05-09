059b4b6741f15eab2857c8227a075e62c7005311
KonstantYa <lakaste@bk.ru>
2023-05-09 18:14:47 +0300

    add exercise_3

--- lesson#5/Program.cs
+++ lesson#5/Program.cs
@@ -1,19 +1,60 @@
 ﻿
-int[] numArr = new int[12];
-int positiveNum=0;
-int negNum=0;
+// int[] numArr = new int[12];
+// int positiveNum=0;
+// int negNum=0;
+// for(int i=0;i<numArr.Length;i++)
+// {
+//    numArr[i] = new Random().Next(-9, 10);
+
+//    if(numArr[i]>0)
+//    {
+//       positiveNum+=numArr[i];
+//    }
+//    else
+//    {
+//       negNum+=numArr[i];
+//    }
+// }
+
+// Console.WriteLine(String.Join(",", numArr)+ " \nсумма положительных "+positiveNum+" сумма отрицательных "+ negNum);
+
+
+
+// int[] numArr = new int[]{1, 7898,66,-6,-52,0};
+
+// Console.WriteLine(String.Join(",", numArr));
+
+// for(int i=0;i<numArr.Length;i++)
+// {
+//    numArr[i]=-numArr[i];
+// }
+
+// Console.WriteLine(String.Join(",", numArr));
+
+
+Console.WriteLine("Enter a number");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] numArr = new int[] {1,345,12,34,56};
+Console.WriteLine(String.Join(",", numArr));
+bool flag = false;
+
 for(int i=0;i<numArr.Length;i++)
 {
-   numArr[i] = new Random().Next(-9, 10);
-
-   if(numArr[i]>0)
+   if (numArr[i]==num)
    {
-      positiveNum+=numArr[i];
+flag=true;
+break;
    }
-   else
-   {
-      negNum+=numArr[i];
-   }
+   
 }
 
-Console.WriteLine(String.Join(",", numArr)+ " \nсумма положительных "+positiveNum+" сумма отрицательных "+ negNum);
+if(flag==true)
+
+{
+Console.WriteLine("Yes");
+
+}
+else 
+{
+   Console.WriteLine("No");
+}
